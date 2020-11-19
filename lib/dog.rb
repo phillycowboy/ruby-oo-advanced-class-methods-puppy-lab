d164c53e88a54540a6513c4db14478c5f1c2954a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-19 13:49:54 -0500

    commit tree 1d6febc207319388582e486f569b765b650c2237

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+require 'pry'
+
+class Dog
+@@all = []
+attr_accessor :name
+    def initialize(name)
+        @name = name 
+        save 
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+    
+    # binding.pry 
+    def self.print_all
+        @@all.each do |dog|
+         puts "#{dog.name}"
+
+        end
+    end
+
+    def save 
+        @@all << self 
+
+    end
+
+
+end
